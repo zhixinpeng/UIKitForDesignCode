10e1d9428e31a3768a478065cd440bbf568afb05
彭智鑫 <pengzhixin@live.cn>
2022-07-20 18:11:44 +0800

    feat: finish session 11

--- UIKitForDesignCode/UIKitForDesignCode/Cells/CoursesTableViewCell.swift
+++ UIKitForDesignCode/UIKitForDesignCode/Cells/CoursesTableViewCell.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class CoursesTableViewCell: UITableViewCell {
-
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subtitleLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var courseBackground: UIImageView!
+    @IBOutlet var courseBanner: UIImageView!
+    @IBOutlet var courseLogo: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
--- UIKitForDesignCode/UIKitForDesignCode/ViewControllers/FeatureViewController.swift
+++ UIKitForDesignCode/UIKitForDesignCode/ViewControllers/FeatureViewController.swift
@@ -6,15 +6,30 @@
 //
 
 import UIKit
+import Combine
 
 class FeatureViewController: UIViewController {
     @IBOutlet var handbookCollectionView: UICollectionView!
+    @IBOutlet var coursesTableView: UITableView!
+    @IBOutlet var tableViewHeight: NSLayoutConstraint!
+    
+    private var tokens: Set<AnyCancellable> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         handbookCollectionView.delegate = self
         handbookCollectionView.dataSource = self
         handbookCollectionView.layer.masksToBounds = false
+        
+        coursesTableView.delegate = self
+        coursesTableView.dataSource = self
+        coursesTableView.layer.masksToBounds = false
+        
+        coursesTableView.publisher(for: \.contentSize)
+            .sink { contentSize in
+                self.tableViewHeight.constant = contentSize.height
+            }
+            .store(in: &tokens)
     }
 }
 
@@ -26,6 +41,7 @@ extension FeatureViewController: UICollectionViewDelegate, UICollectionViewDataS
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CourseCell", for: indexPath) as! HandbookCollectionViewCell
         let handbook = handbooks[indexPath.item]
+        
         cell.titleLabel.text = handbook.courseTitle
         cell.subtitleLabel.text = handbook.courseSubtitle
         cell.descriptionLabel.text = handbook.courseDescription
@@ -35,7 +51,36 @@ extension FeatureViewController: UICollectionViewDelegate, UICollectionViewDataS
         
         return cell
     }
-    
-    
 }
 
+extension FeatureViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return courses.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CoursesTableCell", for: indexPath) as! CoursesTableViewCell
+        let course = courses[indexPath.section]
+        
+        cell.titleLabel.text = course.courseTitle
+        cell.subtitleLabel.text = course.courseSubtitle
+        cell.descriptionLabel.text = course.courseDescription
+        cell.courseBackground.image = course.courseBackground
+        cell.courseBanner.image = course.courseBanner
+        cell.courseLogo.image = course.courseIcon
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return section == 0 ? 0 : 20
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0
+    }
+}
