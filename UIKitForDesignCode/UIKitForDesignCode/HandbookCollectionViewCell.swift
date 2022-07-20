2a91636d93e08283463cce92a94e4943ead75227
彭智鑫 <pengzhixin@live.cn>
2022-07-20 11:35:14 +0800

    feat: finish session 3

---
+++ UIKitForDesignCode/UIKitForDesignCode/HandbookCollectionViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  HandbookCollectionViewCell.swift
+//  UIKitForDesignCode
+//
+//  Created by 彭智鑫 on 2022/7/19.
+//
+
+import UIKit
+
+class HandbookCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var overlay: UIView!
+    @IBOutlet var banner: UIImageView!
+    @IBOutlet var logo: CustomImageView!
+    @IBOutlet var progressBar: UIProgressView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subtitleLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    let gradient = CAGradientLayer()
+
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        super.layoutIfNeeded()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.shadowColor = UIColor(named: "Shadow")!.cgColor
+        layer.shadowRadius = 10
+        layer.shadowOpacity = 0.25
+        layer.shadowOffset = CGSize(width: 0, height: 5)
+        layer.masksToBounds = false
+        layer.cornerRadius = 30
+        layer.cornerCurve = .continuous
+        
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 1, y: 1)
+        gradient.frame = overlay.frame
+        gradient.cornerCurve = .continuous
+        gradient.cornerRadius = 30
+    }
+}
--- UIKitForDesignCode/UIKitForDesignCode/ViewController.swift
+++ UIKitForDesignCode/UIKitForDesignCode/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+}
 
-
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CourseCell", for: indexPath) as! HandbookCollectionViewCell
+        cell.titleLabel.text = "SwiftUI Handbook"
+        cell.subtitleLabel.text = "20 HOURS - 30 SECTIONS"
+        cell.descriptionLabel.text = "Learn about all the basics of SwiftUI"
+        cell.gradient.colors = [UIColor.red.cgColor, UIColor.systemPink.cgColor]
+        cell.logo.image = UIImage(named: "Logo React")
+        cell.banner.image = UIImage(named: "Illustration 2")
+        
+        return cell
+    }
+    
+    
 }
 
