ce02ef143464cda9fdef726c0cfae889ceac4d88
彭智鑫 <pengzhixin@live.cn>
2022-07-20 22:09:57 +0800

    feat: finish session 15

--- UIKitForDesignCode/UIKitForDesignCode/Cells/SectionTableViewCell.swift
+++ UIKitForDesignCode/UIKitForDesignCode/Cells/SectionTableViewCell.swift
@@ -1,5 +1,5 @@
 //
-//  CoursesTableViewCell.swift
+//  SectionTableViewCell.swift
 //  UIKitForDesignCode
 //
 //  Created by 彭智鑫 on 2022/7/20.
@@ -7,23 +7,14 @@
 
 import UIKit
 
-class CoursesTableViewCell: UITableViewCell {
+class SectionTableViewCell: UITableViewCell {
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var subtitleLabel: UILabel!
     @IBOutlet var descriptionLabel: UILabel!
-    @IBOutlet var courseBackground: UIImageView!
-    @IBOutlet var courseBanner: UIImageView!
     @IBOutlet var courseLogo: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
-        layer.shadowColor = UIColor(named: "Shadow")!.cgColor
-        layer.shadowOpacity = 0.5
-        layer.shadowOffset = CGSize(width: 0, height: 5)
-        layer.shadowRadius = 10
-        layer.masksToBounds = false
-        layer.cornerRadius = 30
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- UIKitForDesignCode/UIKitForDesignCode/ViewControllers/CoursesViewController.swift
+++ UIKitForDesignCode/UIKitForDesignCode/ViewControllers/CoursesViewController.swift
@@ -6,12 +6,73 @@
 //
 
 import UIKit
+import Combine
 
 class CoursesViewController: UIViewController {
     var course: Course?
+    private var tokens: Set<AnyCancellable> = []
 
+    @IBOutlet var sectionTableView: UITableView!
+    @IBOutlet var tableViewHeight: NSLayoutConstraint!
+    @IBOutlet var bannerImage: UIImageView!
+    @IBOutlet var backgroundImage: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var authorLabel: UILabel!
+    @IBOutlet var subtitleLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+//    @IBOutlet var iconImageView: CustomImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("This course is \(course?.courseTitle)")
+        
+        sectionTableView.delegate = self
+        sectionTableView.dataSource = self
+        sectionTableView.rowHeight = UITableView.automaticDimension
+        sectionTableView.estimatedRowHeight = UITableView.automaticDimension
+        sectionTableView.publisher(for: \.contentSize)
+            .sink { contentSize in
+                self.tableViewHeight.constant = contentSize.height
+            }
+            .store(in: &tokens)
+        
+//        self.iconImageView.image = course?.courseIcon
+        self.bannerImage.image = course?.courseBanner
+        self.backgroundImage.image = course?.courseBackground
+        self.titleLabel.text = course?.courseTitle
+        self.subtitleLabel.text = course?.courseSubtitle
+        self.descriptionLabel.text = course?.courseDescription
+        self.authorLabel.text = "Taught by \(course?.courseAuthor?.formatted(.list(type: .and, width: .standard)) ?? "Angus")"
     }
+    
+    @IBAction func goBack(_ sender: Any) {
+        dismiss(animated: true)
+    }
+}
+
+extension CoursesViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.course?.sections?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SectionCell", for: indexPath) as! SectionTableViewCell
+        if let selectedCourse = course {
+            let selectedSection = selectedCourse.sections![indexPath.row]
+            cell.titleLabel.text = selectedSection.sectionTitle
+            cell.subtitleLabel.text = selectedSection.sectionSubtitle
+            cell.descriptionLabel.text = selectedSection.sectionDescription
+            cell.courseLogo.image = selectedSection.sectionIcon
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
 }
